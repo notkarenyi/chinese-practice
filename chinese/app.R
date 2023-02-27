5600139cc1e2fbc246bf990469fb082b3c9d9c16
electricdinosaurs <notkarenyi@gmail.com>
2023-02-26 16:35:29 -0800

    give up on the sentence generation
    I learned a lot but then I thought about chatgpt lol

--- chinese/app.R
+++ chinese/app.R
@@ -7,84 +7,98 @@ library(topicmodels)
 library(NLP)
 library(openNLP)
 library(data.table)
+library(SnowballC)
 
 ####### READ DATA
 sentences <- read.csv("sentences.csv")
-sentences <- sentences[[1]]
+independent <- sentences$independent
+independent <- independent[independent!=""]
+dependent <- sentences$dependent
 
-vocab <- read_csv("chinese/vocab.csv")
-View(vocab)
-
+vocab <- read_csv("vocab.csv")
 
 sentAnnotator <- Maxent_Sent_Token_Annotator(language="en",probs=TRUE,model=NULL)
 wordAnnotator <- Maxent_Word_Token_Annotator(language="en",probs=TRUE,model=NULL)
 posAnnotator <- Maxent_POS_Tag_Annotator(language="en",probs=TRUE,model=NULL)
 
-sentence <- vocab$english
-sentence <- map(sentence, ~paste0(toupper(substr(.,0,1)),
-                                  substr(.,2,nchar(.))))
+pos <- function(sentence) {
+    results <- annotate(sentence,
+                        posAnnotator,
+                        annotate(sentence,
+                                 wordAnnotator,
+                                 annotate(sentence,
+                                          sentAnnotator)))
+    
+    results <- results$features %>% unlist()
+    results <- results[names(results)=="POS"] %>% unname()
+    results <- chunk(results)
+    return(results)
+}
 
-sentence <- paste0(sentence,collapse=". ")
+chunk <- function(x) {
+    # print(x)
+    # print(sum(grep("VBP",x)))
+    if (sum(grep("VBP",x))) {
+        return("verb")
+    } else if (sum(grep("JJ",x))) {
+        return("adjective")
+    } else {
+        return("noun")
+    }
+}
 
-results <- annotate(sentence,
-                    posAnnotator,
-                    annotate(sentence,
-                             wordAnnotator,
-                             annotate(sentence,
-                                      sentAnnotator)))
+vocab <- as.data.table(vocab)
+vocab <- vocab[,pos:=pos(english),by="chinese"]
 
-words <- results[results$type=="word"]$features %>% unlist()
-words <- words[names(words)=="POS"] %>% unname()
-words[words=="."] <- ""
-words <- data.frame(sentences = 1:length(words),words)
-
-sentences <- results[results$type=="sentence"]$features %>% unlist()
-sentences <- data.frame(sentences, name = names(sentences)) %>%
-    filter(name!="prob") %>%
-    mutate(sep = name=="constituents1") %>%
-    mutate(sentences = sentences - 121) %>%
-    as.data.table()
-sentences <- sentences[,id:=cumsum(sep)]
-sentences <- sentences[,!c("sep")]
-sentences <- left_join(sentences,words) %>%
-    filter(words!="")
-sentences <- sentences[,.(list(words)),by="id"]
-
-# words <- words[grep("[A-Z]",words)]
-
-parts <- c("noun", "verb")
+parts <- c("noun","verb","adjective")
 
 ####### CLEAN DATA
 
 vocab$english <- map(vocab$english, trimws)
-vocab$english <- map(vocab$english, ~gsub("â€™", ., "\'"))
+# vocab$english <- map(vocab$english, ~gsub("â€™", ., "\'"))
+# remove phrases
+vocab <- vocab[nchar(english)<20,]
+# vocab <- vocab[,english:=gsub("[,;(].*","",english),by="chinese"]
 
 ####### APP
 ui <- fluidPage(
     h1("Chinese Practice"),
     verbatimTextOutput("prompt"),
     
-    textInput("caption","Translate this sentence")
+    textInput("caption","Translate this sentence"),
+    
+    actionButton("submit", "Submit")
 )
 
 server <- function(input, output) {
-    output$prompt <- renderText({ 
-        s <- sentences[sample(1:length(sentences),1)]
+    
+    observeEvent(input$submit, {output$prompt <- renderText({
+        # get a sentence template
+        s <- independent[sample(1:length(independent),1)]
+        if (sample(c(1,2),1)==1) {
+            s <- paste0(dependent[sample(1:length(dependent),1)], ", ", s)
+        }
         print(s)
-        s <- strsplit(s, " ")[[1]]
-
-        for (j in parts) {
-            subset <- filter(vocab, speech==j)
-            for (i in grep(j,s))
-                s[i] <- sub(j,subset$english[sample(1:nrow(subset),1)],s[i])
+        s <- unlist(strsplit(s, " "))
+        
+        # mad lib the words using the vocab list
+        for (part in parts) {
+            subset <- filter(vocab, pos==part)
+            for (word in grep(part,s))
+                s[word] <- sub(part,
+                               subset$english[sample(1:nrow(subset),1)],
+                               s[word])
         }
         
+        # form the sentence
+        s <- paste0(paste0(s,collapse = " "), ".")
+    
         #capitalize first letter
-        s[0] <- toupper(s[0])
+        s <- paste0(toupper(substr(s,0,1)), substr(s,2,nchar(s)))
         
         #print
         s
-    })
+    })})
 }
 
 shinyApp(ui, server)
