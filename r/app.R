01b826266064c1a1f606cfa7f808fe5dc541325c
Karen <notkarenyi@gmail.com>
2024-12-26 18:01:36 -0700

    emily says its too overwhelming
    only show level 1 of graph

--- r/app.R
+++ r/app.R
@@ -126,7 +126,7 @@ server <- function(input, output) {
         # get all phrases with the root character
         # input = c()
         # input$root = '不'
-        stop = 2
+        stop = 1
         root_phrases <- unlist(unname(chars[v==input$root,"pos"]))
         graph_results <- get_nodes(root_phrases, counter=1, stop=stop, color=1, orig_root=root_phrases)
 
